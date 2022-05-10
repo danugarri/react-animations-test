4aaf56f8d8cc19dbe8427d39f97eacba6a604c71
danugarri <danugarri@hotmail.com>
2022-05-10 23:01:26 +0200

    handling stopPropagation for modal click

--- src/components/modalTest/ModalTest.jsx
+++ src/components/modalTest/ModalTest.jsx
@@ -4,10 +4,11 @@ import './modalTest.css';
 export const ModalTest = (props) => {
     // HERE we use the prop children
     const {children, closeModal, isOpen} = props;
+    const handleCloseModalEvent = e => e.stopPropagation();
     
   return (
     <section className={`modal ${isOpen && 'is-open'}`} onClick= {closeModal}>
-        <div className="modal-container">
+        <div className="modal-container" onClick= {handleCloseModalEvent}>
            <button className="modal-close" onClick= {closeModal}>X</button>
             {children}
         </div>
